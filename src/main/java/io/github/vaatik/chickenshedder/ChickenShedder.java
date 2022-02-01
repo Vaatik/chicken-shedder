dc9c1065b18cd30b9ee78be163e41d005777bac6
Vaatik <sbilodeau.web@gmail.com>
2022-01-31 22:37:09 -0500

    Updated to v.1.0.1

--- src/main/java/io/github/vaatik/chickenshedder/ChickenShedder.java
+++ src/main/java/io/github/vaatik/chickenshedder/ChickenShedder.java
@@ -5,12 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ChickenShedder extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Chicken Shedder 1.18.1 enabled!");
+        getLogger().info("Chicken Shedder 1.18 enabled");
         new ChickenShedderListener(this);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("onDisabled is called!");
+        getLogger().info("Chicken Shedder 1.18 disabled");
     }
 }
