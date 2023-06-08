60c25523eb08aabc98e9c0c8289ede4a343ca197
Vaatik <sbilodeau.web@gmail.com>
2023-06-07 21:42:28 -0400

    update 1.20

---
+++ src/main/java/io/github/vaatik/chickenshedder/ChickenShedder.java
@@ -0,0 +1,21 @@
+package io.github.vaatik.chickenshedder;
+
+import org.bukkit.inventory.Recipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChickenShedder extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        getLogger().info("Chicken Shedder 1.2.3 for Minecraft Spigot 1.20 enabled");
+        new ChickenShedderListener(this);
+
+        ZombieFurnaceRecipe leatherRecipe = new ZombieFurnaceRecipe(this);
+        this.getServer().addRecipe(ZombieFurnaceRecipe.registerNewRecipe());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Chicken Shedder 1.2.3 for Minecraft Spigot 1.20 disabled");
+    }
+}
